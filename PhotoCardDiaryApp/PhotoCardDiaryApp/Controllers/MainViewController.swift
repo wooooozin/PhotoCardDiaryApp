d79d4ac12dca7d202e8fb53f9e0faa7e1a53b305
wooooozin <wooooozin@gmail.com>
2022-10-12 10:46:42 +0900

    [feat] 프로젝트 생성 및 타켓 설정

---
+++ PhotoCardDiaryApp/PhotoCardDiaryApp/Controllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  PhotoCardDiaryApp
+//
+//  Created by 효우 on 2022/10/12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGreen
+    }
+}
